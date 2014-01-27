d2f2971e8b44dfe23c3d96b41c75a79cd79a293e
Maksym Shcheglov <maxscheglov@gmail.com>
2014-01-27 14:00:00 +0100

    Changed demo project structure.

--- MSColorPickerDemo/MSColorPickerDemo/MSAppDelegate.m
+++ MSColorPickerDemo/MSColorPickerDemo/MSAppDelegate.m
@@ -7,15 +7,19 @@
 //
 
 #import "MSAppDelegate.h"
+#import "MSViewController.h"
 
 @implementation MSAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    return YES;
+  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:[[MSViewController alloc] init]];
+  self.window.rootViewController = navigationController;
+  [self.window makeKeyAndVisible];
+  return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
