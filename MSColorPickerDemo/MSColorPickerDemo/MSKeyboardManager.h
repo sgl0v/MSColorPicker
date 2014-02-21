a33a0ca8b41a1f7fb2be97a47ee2326528f5d836
Maksym Shcheglov <maxscheglov@gmail.com>
2014-02-21 17:05:00 +0100

    Implemented keyboard manager.

---
+++ MSColorPickerDemo/MSColorPickerDemo/MSKeyboardManager.h
@@ -0,0 +1,13 @@
+//
+//  MSKeyboardManager.h
+//  MSColorPickerDemo
+//
+//  Created by Maksym Shcheglov on 08/05/14.
+//  Copyright (c) 2014 Maksym Shcheglov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSKeyboardManager : NSObject
+
+@end
---
+++ MSColorPickerDemo/MSColorPickerDemo/MSKeyboardManager.m
@@ -0,0 +1,13 @@
+//
+//  MSKeyboardManager.m
+//  MSColorPickerDemo
+//
+//  Created by Maksym Shcheglov on 08/05/14.
+//  Copyright (c) 2014 Maksym Shcheglov. All rights reserved.
+//
+
+#import "MSKeyboardManager.h"
+
+@implementation MSKeyboardManager
+
+@end
