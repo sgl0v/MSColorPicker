02639161ddc5c6eb29c8a6c4799667c22b0ef127
Maksym Shcheglov <maxscheglov@gmail.com>
2015-04-04 00:15:00 +0100

    Updated files headers, added missed files.

--- MSColorPicker/MSRGBView.h
+++ MSColorPicker/MSRGBView.h
@@ -1,10 +1,9 @@
 //
-// MSSliderView.h
-// MSColorPickerDemo
+// MSRGBView.h
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2014 Maksym Shcheglov
+// Copyright (c) 2015 Maksym Shcheglov
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +22,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
 
 #import <UIKit/UIKit.h>
 
--- MSColorPickerDemo/MSColorPickerDemo/Supporting Files/main.m
+++ MSColorPickerDemo/MSColorPickerDemo/Supporting Files/main.m
@@ -1,10 +1,9 @@
 //
 // main.m
-// MSColorPickerDemo
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2014 Maksym Shcheglov
+// Copyright (c) 2015 Maksym Shcheglov
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
