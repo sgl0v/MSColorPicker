62dff8854e5e39c30ff734b1955d6fdfd0728987
Maksym Shcheglov <maxscheglov@gmail.com>
2015-04-06 12:11:00 +0100

    Removed leftovers from the MSViewController class.

--- MSColorPickerDemo/MSColorPickerDemo/MSViewController.m
+++ MSColorPickerDemo/MSColorPickerDemo/MSViewController.m
@@ -1,103 +1,53 @@
 //
-// MSViewController.m
+//  MSViewController.m
+//  MSColorPickerDemo
 //
-// The MIT License (MIT)
+//  Created by Maksym Shcheglov on 03/04/15.
+//  Copyright (c) 2015 Maksym Shcheglov. All rights reserved.
 //
-// Copyright (c) 2015 Maksym Shcheglov
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
 
 #import "MSViewController.h"
-#import <MSColorPicker/MSColorPicker.h>
 
-typedef NS_ENUM (NSUInteger, MSSelectedColorView) {
-    MSSelectedColorViewRGB,
-    MSSelectedColorViewHSB
-};
-
-@interface MSViewController () <MSColorViewDelegate>
-
-@property (nonatomic, strong) UIView<MSColorView> *currentColorView;
-@property (nonatomic, strong) UIView<MSColorView> *rgbColorView;
-@property (nonatomic, strong) UIView<MSColorView> *hsbColorView;
-@property (nonatomic, weak) IBOutlet UISegmentedControl *segmentedControl;
-@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
-@property (nonatomic, strong) UIColor *selectedColor;
+@interface MSViewController () <UIPopoverPresentationControllerDelegate>
 
 @end
 
 @implementation MSViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-    self.selectedColor = [UIColor whiteColor];
-    [self ms_updateContentWithSelectedView:self.segmentedControl.selectedSegmentIndex];
+    // Do any additional setup after loading the view.
 }
 
-- (IBAction)segmentControlDidChangeValue:(UISegmentedControl *)sender
-{
-    [self ms_updateContentWithSelectedView:self.segmentedControl.selectedSegmentIndex];
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
-- (UIView<MSColorView> *)rgbColorView
-{
-    if (!_rgbColorView) {
-        _rgbColorView = [[MSRGBView alloc] init];
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Assuming you've hooked this all up in a Storyboard with a popover presentation style
+    if ([segue.identifier isEqualToString:@"showPopover"]) {
+        UINavigationController *destNav = segue.destinationViewController;
+        destNav.preferredContentSize = [[destNav visibleViewController].view systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+        destNav.popoverPresentationController.delegate = self;
+        if (self.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClassCompact) { // Add done button for the compact size
+            UIBarButtonItem *doneBtn = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStyleDone target:self action:@selector(dismissViewController:)];
+            destNav.visibleViewController.navigationItem.rightBarButtonItem = doneBtn;
+        }
     }
-
-    return _rgbColorView;
 }
 
-- (UIView<MSColorView> *)hsbColorView
-{
-    if (!_hsbColorView) {
-        _hsbColorView = [[MSHSBView alloc] init];
-    }
+#pragma mark - UIAdaptivePresentationControllerDelegate methods
 
-    return _hsbColorView;
+- (UIModalPresentationStyle)adaptivePresentationStyleForPresentationController:(UIPresentationController *)controller {
+    return UIModalPresentationFullScreen;
 }
 
-#pragma mark - FBColorViewDelegate methods
+#pragma mark - Private
 
-- (void)colorView:(id<MSColorView>)colorView didChangeValue:(UIColor *)colorValue
+- (void)dismissViewController:(id)sender
 {
-    self.selectedColor = colorValue;
-}
-
-#pragma mark - Private methods
-
-- (void)ms_updateContentWithSelectedView:(MSSelectedColorView)selectedView
-{
-    self.currentColorView.delegate = nil;
-    [self.currentColorView removeFromSuperview];
-    self.currentColorView = selectedView == MSSelectedColorViewRGB ? self.rgbColorView : self.hsbColorView;
-    self.currentColorView.delegate = self;
-    [self.scrollView addSubview:self.currentColorView];
-
-    self.currentColorView.translatesAutoresizingMaskIntoConstraints = NO;
-    NSDictionary *views = NSDictionaryOfVariableBindings(_currentColorView);
-    [self.scrollView addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.currentColorView attribute:NSLayoutAttributeWidth multiplier:1.0f constant:0.0f]];
-    [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_currentColorView]|" options:0 metrics:nil views:views]];
-    [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_currentColorView]|" options:0 metrics:nil views:views]];
-
-    self.currentColorView.value = self.selectedColor;
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
