ba4b88ccf2854e10e4697208c33476e0e9d063d8
Maksym Shcheglov <maxscheglov@gmail.com>
2015-04-11 17:05:00 +0100

    Added comments.

--- MSColorPickerDemo/MSColorPickerDemo/MSColorSelectionView.h
+++ MSColorPickerDemo/MSColorPickerDemo/MSColorSelectionView.h
@@ -11,17 +11,29 @@
 @protocol MSColorView;
 
 typedef NS_ENUM (NSUInteger, MSSelectedColorView) {
-    MSSelectedColorViewRGB,
-    MSSelectedColorViewHSB
+	MSSelectedColorViewRGB,
+	MSSelectedColorViewHSB
 };
 
+/**
+ *  The MSColorSelectionView aggregates views that should be used to edit color components.
+ */
 @interface MSColorSelectionView : UIView
 
 @property (nonatomic, assign) MSSelectedColorView selectedIndex;
-@property (nonatomic, strong) UIView<MSColorView> *rgbColorView;
-@property (nonatomic, strong) UIView<MSColorView> *hsbColorView;
+//! @abstract This view represents rgb color components
+@property (nonatomic, strong) UIView <MSColorView> *rgbColorView;
+//! @abstract This view represents hsb color components
+@property (nonatomic, strong) UIView <MSColorView> *hsbColorView;
+//! @abstract The selected color
 @property (nonatomic, strong) UIColor *selectedColor;
 
+/**
+ *  Makes a color component view (rgb or hsb) visible according to the index.
+ *
+ *  @param index    This index define a view to show.
+ *  @param animated If YES, the view is being appeared using an animation.
+ */
 - (void)setSelectedIndex:(MSSelectedColorView)index animated:(BOOL)animated;
 
 @end
