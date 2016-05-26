7f8db515301e288420f463a28e16330bc738656c
Maksym Shcheglov <maxscheglov@gmail.com>
2016-05-27 00:21:00 +0100

    Updated the project structure.

---
+++ MSColorPicker/Supporting Files/main.m
@@ -0,0 +1,18 @@
+//
+// main.m
+//
+// Created by Maksym Shcheglov on 2016-05-31.
+// Copyright (c) 2016 Maksym Shcheglov.
+// License: http://opensource.org/licenses/MIT
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+
+pod "MSColorPicker", :path => "./"
+
+target :MSColorPickerDemoTests do
+  pod "MSColorPicker", :path => "./"
+end
